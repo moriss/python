5485c74d2ca2ed42847e56424a47dc7540301f67
morris-bedroom <marrocco001@gmail.com>
2024-07-10 20:20:14 +0100

    all

--- mytests/try.py
+++ mytests/try.py
@@ -1,12 +1,15 @@
 #You are onto page 2 of your notes
-#Convert 99 to a floating point number
-#Convert a variable with a vvalue of 99 to a floating point number
+ #Convert 99 to a floating point number
+ #Convert a variable with a vvalue of 99 to a floating point number
  #Convert a string (of numbers) to an integer
  #Ask someone for their name and print  hello  name.
  #divide two variables
-ee=input('Choose your first number to divide with ');
-ee=int(ee);
-print(type(ee));
+#Right a program that converts to US floor numbers from EU floor numbers
+euqueery =input("What eu floor number do you wish? ");
+eu=int(euqueery);
+us = eu+1;
+
+print("The us floor number is" , us);
 
 
 
