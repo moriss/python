412e5bcca050ac1a3328043e7b2d5912a6cf179b
morris-bedroom <marrocco001@gmail.com>
2024-08-26 20:37:36 +0100

    all

---
+++ try.py
@@ -0,0 +1,13 @@
+#find out what 'type' some data is.
+#You are onto page 2 of your notes
+#Take a number and decide if its over or under a specified number 
+	#Prompt user for work hours + rate p/hr to calculate their gross pay
+#Convert 99 to a floating point number
+#Convert a variable with a value of 99 to a floating point number
+#Convert a string (of numbers) to an integer
+#Ask someone for their name and print  hello  name.
+#divide two variables
+#Right a program that converts to US floor numbers from EU floor numbers
+
+a = '14'
+print(type(a))
