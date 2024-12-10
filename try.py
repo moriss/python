c1b4843648ee3eef41df6aeedd1df3a8fb20cc03
morris-bedroom <marrocco001@gmail.com>
2024-12-10 20:29:13 +0000

    all

--- try.py
+++ try.py
@@ -1,10 +1,11 @@
+
 #DO THE ONE WITH THE LINE SPACE ABOVE.
 
 #MF:1 Create a function that greets someone in their own LANGUAGE.2345
 #MF: 12  Use MAX to find the largest value character2345
+#MF:12  Find out what TYPE some data is.345
+#MF:12 Test user's number is under specified THRESHOLDS with elif. 345
 
-#MF:1  Find out what 'TYPE' some data is.23
-#mf:1 Test user's number is under specified THRESHOLDS with elif. 23
 #MF:1 Calculate hourly rate with time and a half over 40 hours. 23
 #MF 123 Convert 99 to a FLOATING point number
 # 3 MF Convert a string(numbers) to an integer 3 STRING_TO_INTEGER
