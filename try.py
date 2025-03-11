8244818d9d1900a0867060ff48f68a4d3ac34c88
morris-bedroom <marrocco001@gmail.com>
2025-03-11 19:29:52 +0000

    all

--- try.py
+++ try.py
@@ -1,21 +1,29 @@
-
 #HOLIDAY REVISION HAS NOW ENDED. DELETE REFERENCES TO HOLIDAY or H AS YOU MOVE THROUGHT THE EXERCISES.
 
-# 2Use return values to greet people in there own language. LANGUAGE_RETURN.3456
-# MF:12 Use the print option to greet people in their own LANGUAGE.2345
+#  2 Use TRYANDEXCEPT to catch a value error. 2345
+def atn(stump):
+	try: 
+		print(stump-1)
+	except: 
+		print("Invalid input")
+	quit()
+d=atn(input("type a number"))
+print(d)
+
+
+# 2Use return values to greet people in there own language. LANGUAGE_RETURN.3456# MF:12 Use the print option to greet people in their own LANGUAGE.2345
 #MF:12  Use MAX to find the largest value character345
 #MF:123  Find out what TYPE some data is.45
 #MF:123  Test user's number is under specified THRESHOLDS with elif. 45 
+#MF: 1 Calculate HOURLY-RATE with time and a half over 40 hours.2345
+#MF 1234 Convert a value to a FLOATING point number  5
+# 4 MF Convert a string(numbers) to an integer STRING_TO_INTEGER 5
+#MF 1234 Ask someone for their name and print HELLO-NAME. 5
+# 2 CALCULATE two variables 2345
+#  2Convert to US FLOOR_NUMBER from EU floor numbers 23456
+#  3 Use TRYANDEXCEPT to catch a value error. 345
 
-#MF:(holiday1) 1 Calculate HOURLY RATE with time and a half over 40 hours.(h)2345
-#MF (HOLIDAY1) 1234 Convert a value to a FLOATING point number (H) 5
-# (HOLIDAY1) 3 MF Convert a string(numbers) to an integer STRING_TO_INTEGER (H) 45
-#MF (HOLIDAY1) 123 Ask someone for their name and print HELLO-NAME. (H) 45
-#MF (HOLIDAY1) 1 CALCULATE two variables 12345  (H)
-#  DConvert to US floor numbers from EU floor numbers 
-#  1 Use TRYANDEXCEPT to catch a value error. 2345
-# 2Use return values to greet people in there own language. LANGUAGE_RETURN.3456
-#2Worked ex.4.6 COMPUTEPAY is a two-parameter function that calculates time and a half weekly pay1345
-
+# 2Use return values to greet people in there own language. LANGUAGE_RETURN.3456#2Worked ex.4.6 COMPUTEPAY is a two-parameter function that calculates time and a half weekly pay1345
 
 
+#MF: 12 Calculate HOURLY RATE with time and a half over 40 hours.2345
