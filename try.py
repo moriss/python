8c44e7183e35dbf43dabde3910adc9263079eacc
morris-bedroom <marrocco001@gmail.com>
2025-04-30 20:44:31 +0100

    all

--- try.py
+++ try.py
@@ -4,8 +4,6 @@
 #TYPE YOUR CODE HERE
 
 
-
-
 # 4 Use the print option to  greet people in there own language. LANGUAGE.456
 # MF:12 Use the return option to greet people in their own LANGUAGE_RETURN.2345
 #MF: 4 Use MAX to find the largest value character Level:456 Version:V1
@@ -14,9 +12,9 @@
 #MF: 2 Calculate HOURLY-RATE with time and a half over 40 hours.2345 V1
 #MF 1 Convert a value to a FLOATING point number 1234 5 v2
 # 1 MF Convert a string(numbers) to an integer STRING_TO_INTEGER 12345 v2
+#MF 1 Ask someone for their name and print HELLO-NAME. 12345 v2
 
-#MF 1234 Ask someone for their name and print HELLO-NAME. 5
-# 2 CALCULATE two variables 2345
+# 2 CALCULATE two variables 2345 v1
 #  2Convert to US FLOOR_NUMBER from EU floor numbers 23456
 #  3 Use TRYANDEXCEPT to catch a value error. 345
 #3 Worked ex.4.6 COMPUTEPAY is a two-parameter function that calculates time and a half weekly pay345
