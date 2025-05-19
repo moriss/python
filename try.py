4de2048dbcc11d6730a9c8ac25bfdf0ba0acf569
morris-bedroom <marrocco001@gmail.com>
2025-05-19 20:19:04 +0100

    all

--- try.py
+++ try.py
@@ -1,21 +1,26 @@
+#current video https://www.youtube.com/watch?v=FzpurxjwmsM&list=PLlRFEj9H3Oj7Bp8-DfGpfAfDBiblRfl5p&index=24
+#STOPPED BEFORE 'BREAK' 6.24 IN
 #Alternate try except code (3) to capture : if not, ValueError and TypeError etc.Take your time and maybe start only with handling an empty string.
 # Create 3 versions of the same code outcome. v1.v2.v3 for each 1 to 5
+#v1=just easy v2=concatination v3=some shorthand
 
 #TYPE YOUR CODE HERE
 
 
-# 4 Use the print option to  greet people in there own language. LANGUAGE.456
-# MF:12 Use the return option to greet people in their own LANGUAGE_RETURN.2345
-#MF: 4 Use MAX to find the largest value character Level:456 Version:V1
-#MF: 5  Find out what TYPE some data is.Level:5 Version:V1
-#MF:1  Test user's number is under specified THRESHOLDS with elif. 12345 V2
-#MF: 2 Calculate HOURLY-RATE with time and a half over 40 hours.2345 V1
-#MF 1 Convert a value to a FLOATING point number 1234 5 v2
-# 1 MF Convert a string(numbers) to an integer STRING_TO_INTEGER 12345 v2
-#MF 1 Ask someone for their name and print HELLO-NAME. 12345 v2
+# 4 Use the print option to  greet people in there own language. LANGUAGE.456 v1
 
-# 2 CALCULATE two variables 2345 v1
-#  2Convert to US FLOOR_NUMBER from EU floor numbers 23456
-#  3 Use TRYANDEXCEPT to catch a value error. 345
-#3 Worked ex.4.6 COMPUTEPAY is a two-parameter function that calculates time and a half weekly pay345
+# 3 Use the return option to greet people in their own LANGUAGE_RETURN.345 v1
+# 4 Use MAX to find the largest value character Level:456 Version:V1
+# 5  Find out what TYPE some data is.Level:5 Version:V1
+# 1  Test user's number is under specified THRESHOLDS with elif. 12345 V2
+# 2 Calculate HOURLY-RATE with time and a half over 40 hours.2345 V1
+# 1 Convert a value to a FLOATING point number 1234 5 v2
+# 1 MF Convert a string(numbers) to an integer STRING_TO_INTEGER 12345 v2
+# 1 Ask someone for their name and print HELLO-NAME. 12345 v2
+# 4 CALCULATE two variables 45 v1
+# 2  Create a COUNTDOWN from n to zero with a loop 2345 v1
+# 3 Convert to US FLOOR_NUMBER from EU floor numbers 3456 v1
+# 4 Use TRYANDEXCEPT to catch a value error. 45 v1
+# 4 Worked ex.4.6 COMPUTEPAY is a two-parameter function that calculates time and a half weekly pay45 v1
+
 
