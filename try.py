b2ec48f6d6ff8dc250b87cb039fb2bdaea6c6d38
morris-bedroom <marrocco001@gmail.com>
2025-06-19 20:26:53 +0100

    all

--- try.py
+++ try.py
@@ -2,27 +2,40 @@
 #STOPPED BEFORE 'BREAK' 6.24 IN
 #Alternate try except code (3) to capture : if not, ValueError and TypeError etc.Take your time and maybe start only with handling an empty string.
 # Create 3 versions of the same code outcome. v1.v2.v3 for each 1 to 5
-#v1=just easy v2=concatination v3=some shorthand
+#v1=just easy v2=concatination v3=some shorthand v4=Fill with existing analytical and manipulative functions
+#STICK WITH V1 UNTIL YOU FEEL CONFIDENT
 
 #TYPE YOUR CODE HERE
-# 1  Find out what TYPE some data is 1 v2
-a=3
-b="Hello"
-d='a'
-print(type(a) , type(b) , type(d))
+# 2 MF Convert a string(numbers) to an integer STRING_TO_INTEGER 2345 v1
+def lepard(split):
+	split = int(split)
+	print(split + 1)
 
+lepard(input("Give me a number"))
 
 # 4 Use the print option to  greet people in there own language. LANGUAGE.456 v1
 # 5 Use MAX to find the largest value character Level: 56 Version:V1
+# 2  Find out what TYPE some data is 2345 v1
+# 2  Test user's number is under specified THRESHOLDS with elif. 2345 V1
+# 3 Calculate HOURLY-RATE with time and a half over 40 hours. 345 V1
+# 3 Convert a value to a FLOATING point number 345 v1
 
-# 1  Find out what TYPE some data is 1 v2
-# 1  Test user's number is under specified THRESHOLDS with elif. 12345 V2
-# 2 Calculate HOURLY-RATE with time and a half over 40 hours.2345 V1
-# 1 Convert a value to a FLOATING point number 1234 5 v2
-# 1 MF Convert a string(numbers) to an integer STRING_TO_INTEGER 12345 v2
-# 1 Ask someone for their name and print HELLO-NAME. 12345 v2
+# 2 MF Convert a string(numbers) to an integer STRING_TO_INTEGER 2345 v1
+# 1 Ask someone for their name and print HELLO-NAME. 12345 v1
 # 4 CALCULATE two variables 45 v1
 # 2  Create a COUNTDOWN from n to zero with a loop 2345 v1
 # 3 Convert to US FLOOR_NUMBER from EU floor numbers 3456 v1
 # 4 Use TRYANDEXCEPT to catch a value error. 45 v1
 # 4 Worked ex.4.6 COMPUTEPAY is a two-parameter function that calculates time and a half weekly pay45 v1
+
+
+#Apply useful/relevant functions below at level 4
+#type() asks for the type of data it is
+#len() Gives the length of data
+#chr() Converts ascii numbers to strings
+
+
+
+#V1= Basic and straightforward
+#V2= Done differently and achieving the same result
+#3= 'Flushing' with multiple available python functions to analyse and manipulate etc the code 
